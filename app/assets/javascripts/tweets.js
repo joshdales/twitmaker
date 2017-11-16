6f123e95f29a41c70c7410ac6e91132c060388e0
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-16 13:58:33 -0500

    Wait for DOM to be fully loaded in tweets.js

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,5 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+document.addEventListener('DOMContentLoaded', function() {
+  var newTweet = document.getElementById('new_tweet');
+})
